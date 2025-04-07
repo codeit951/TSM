68161076f2cf132804a689ceea064c13656f9181
loadedapps <loadedapps@gmail.com>
2025-04-07 21:28:29 +0100

    more

---
+++ TSM.TradingWebApp/Components/Controls/Common/MyToastType.cs
@@ -0,0 +1,10 @@
+﻿namespace TSM.TradingWebApp.Components.Controls.Common
+{
+    public enum MyToastType
+    {
+        Success = 0,
+        Danger = 1,
+        Info = 2,
+        Warning = 3
+    }
+}
---
+++ TSM.TradingWebApp/wwwroot/site.js
@@ -0,0 +1,48 @@
+﻿window.registerOutsideClick = (dotnetHelper) => {
+    document.addEventListener("mousedown", (event) => {
+        let dropdown = document.getElementById("dropdownMenu");
+        let userButton = document.querySelector(".user");
+
+        if (dropdown && !dropdown.contains(event.target) && !userButton.contains(event.target)) {
+            dotnetHelper.invokeMethodAsync("CloseDropdown");
+        }
+
+        let notidown = document.getElementById("notiarea");
+        let notiButton = document.querySelector(".bell_wrapper");
+
+        if (notidown && !notidown.contains(event.target) && !notiButton.contains(event.target)) {
+            dotnetHelper.invokeMethodAsync("CloseTransform");
+        }
+
+        let settingsdown = document.getElementById("settingsarea");
+        let settingsButton = document.querySelector(".settings_link");
+
+        if (settingsdown && !settingsdown.contains(event.target) && !settingsButton.contains(event.target)) {
+            dotnetHelper.invokeMethodAsync("CloseSettingsTransform");
+        }
+
+    });
+};
+window.registerOutsideClick2 = (dotnetHelper) => {
+    document.addEventListener("mousedown", (event) => {
+        
+        let coinsbaldown = document.getElementById("coinsbalarea");
+        let coinsbalButton = document.querySelector(".amount_select");
+
+        if (coinsbaldown && !coinsbaldown.contains(event.target) && !coinsbalButton.contains(event.target)) {
+            dotnetHelper.invokeMethodAsync("CloseCoinsAndBalanceDropdown");
+        }
+    });
+};
+
+window.registerOutsideClick3 = (dotnetHelper) => {
+    document.addEventListener("mousedown", (event) => {
+
+        let settingsdown = document.getElementById("settingsarea");
+        let settingsButton = document.querySelector(".settings_link");
+
+        if (settingsdown && !settingsdown.contains(event.target) && !settingsButton.contains(event.target)) {
+            dotnetHelper.invokeMethodAsync("CloseSettingsTransform");
+        }
+    });
+};
